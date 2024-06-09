adfa7001815f1dadb9c4381033f6c1e695711b90
iamgraypix <charlesartillero@gmail.com>
2024-06-10 01:58:23 +0800

    db credentails

--- bootstrap.php
+++ bootstrap.php
@@ -8,7 +8,7 @@ $container = new Container;
 
 $container->bind(Database::class, function () {
     $config = require base_path('config.php');
-    return new Database($config['database']);
+    return new Database($config['database'], "root", "admin123");
 });
 
 
