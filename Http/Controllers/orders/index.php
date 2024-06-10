bec968807118b4cfda14ef538375594cb04d8c89
iamgraypix <charlesartillero@gmail.com>
2024-06-10 11:28:03 +0800

    fix bug

--- Http/Controllers/orders/index.php
+++ Http/Controllers/orders/index.php
@@ -5,7 +5,8 @@ use Core\Database;
 
 $db = App::resolve(Database::class);
 
-$stocks = $db->query("SELECT items.id, items.name, items.retail, stocks.available  FROM stocks JOIN items ON stocks.item_id = items.id")->get();
+$stocks = $db->query("SELECT items.id, items.name, items.retail, stocks.available  
+FROM stocks JOIN items ON stocks.item_id = items.id WHERE stocks.available > 0")->get();
 
 
 view('orders/index.view.php', [
