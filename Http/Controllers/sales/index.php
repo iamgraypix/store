2a650929e551f281ab344421ddcafe9384914010
iamgraypix <charlesartillero@gmail.com>
2024-06-11 14:34:52 +0800

    sales page

--- Http/Controllers/sales/index.php
+++ Http/Controllers/sales/index.php
@@ -1,3 +1,17 @@
-<?php 
+<?php
 
-view('sales/index.view.php');
+use Core\App;
+use Core\Database;
+
+$db = App::resolve(Database::class);
+
+$capital = $db->query("SELECT SUM(total_listing) AS capital FROM supplies")->find()['capital'];
+
+$sales = $db->query("SELECT SUM(amount) as sales FROM orders")->find()['sales'];
+
+
+view('sales/index.view.php', [
+    'capital' => $capital,
+    'sales' => $sales,
+    'revenue' => $sales - $capital
+]);
--- views/sales/index.view.php
+++ views/sales/index.view.php
@@ -5,11 +5,37 @@
 <!-- content -->
 <main class="mt-5 pt-3">
     <div class="m-4 p-4 shadow rounded-3" style="background-color: white;" id="content">
-        <?php
-        //default page
-        echo "Sales";
-        // include('includes/inc_dashboard.php');
-        ?>
+        <div>
+            <h1 class="display-5 text-center mb-4">Revenue</h1>
+
+            <div class="table-responsive">
+                <table class="table-striped table table-hover table-bordered">
+                    <thead>
+                        <tr>
+                            <th class="table-danger" scope="col">Total of Capital Money</th>
+                            <th class="table-warning" scope="col">Total of Sold Items</th>
+                            <th class="table-success" scope="col">Revenue</th>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td class="table-danger">
+                                <h3>₱ <?php echo number_format($capital, 2); ?></h3>
+                            </td>
+                            <td class="table-warning">
+                                <h3>₱ <?php echo number_format($sales, 2); ?></h3>
+                            </td>
+                            <td class="table-success">
+                                <h3 style="color: green;">₱ <?php echo number_format($revenue, 2); ?></h3>
+                            </td>
+                        </tr>
+
+                    </tbody>
+                </table>
+            </div>
+
+        </div>
     </div>
 </main>
 
