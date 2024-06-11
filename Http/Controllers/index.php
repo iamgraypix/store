ae49963bbc72d7fdbe2e6d5fbc773dc09a088269
iamgraypix <charlesartillero@gmail.com>
2024-06-11 18:18:28 +0800

    dashboard done

--- Http/Controllers/index.php
+++ Http/Controllers/index.php
@@ -1,3 +1,19 @@
-<?php 
+<?php
 
-view('index.view.php');
+use Core\App;
+use Core\Database;
+
+$db = App::resolve(Database::class);
+
+$count_products = $db->query("SELECT COUNT(*) as count FROM items")->find()['count'];
+
+$sales = $db->query("SELECT SUM(amount) as sales FROM orders")->find()['sales'];
+
+$stocks = $db->query("SELECT items.name, items.retail, stocks.* FROM stocks JOIN items ON stocks.item_id = items.id")->get();
+
+
+view('index.view.php', [
+    "sales" => $sales,
+    "count_products" => $count_products,
+    "stocks" => $stocks
+]);
--- views/index.view.php
+++ views/index.view.php
@@ -10,27 +10,10 @@
 
             <div class="row mb-3">
                 <div class="col-sm-4 col-md">
-                    <div class="card text-center bg-primary">
-                        <div class="card-body">
-                            <h1 class="card-title">
-                                <?php
-                                echo $numCustomer;
-                                ?>
-                            </h1>
-                        </div>
-                        <div class="card-footer">
-                            Total Customer
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-4 col-md">
                     <div class="card text-center bg-warning">
                         <div class="card-body">
                             <h1 class="card-title">
-                                <?php
-                                echo $numProducts;
-
-                                ?>
+                               <?= $count_products ?>
                             </h1>
                         </div>
                         <div class="card-footer">
@@ -43,7 +26,7 @@
                         <div class="card-body">
                             <h1 class="card-title">
                                 <?php
-                                echo "₱ " . number_format($sold_items, 2);
+                                echo "₱ " . number_format($sales, 2);
                                 ?>
                             </h1>
                         </div>
@@ -55,7 +38,7 @@
 
             </div>
 
-            <!-- <?php print_r($allCustomer); ?> -->
+           
             <div class="row mb-3">
                 <div class="col-md-12">
                     <div class="card shadow">
@@ -63,55 +46,37 @@
                             <span>
                                 <i class="bi bi-table"></i>
                             </span>
-                            <span>Customer Table</span>
+                            <span>Stocks Table</span>
                         </div>
                         <div class="card-body">
                             <div class="table-responsive">
                                 <table class="table">
                                     <thead>
                                         <tr>
-                                            <th scope="col">ID</th>
-                                            <th scope="col">Name</th>
-                                            <th scope="col">Cash Tendered</th>
-                                            <th scope="col">Change</th>
-                                            <th scope="col">Amount Paid</th>
+                                            <th scope="col">Product Name</th>
+                                            <th scope="col">Price</th>
+                                            <th scope="col">Sold</th>
+                                            <th scope="col">Available</th>
+                                            
                                         </tr>
                                     </thead>
                                     <tbody>
-
-                                        <?php
-
-                                        foreach ($allCustomer as $customer) :
-                                        ?>
-
+                                        <?php foreach ($stocks as $stock): ?>
                                             <tr>
-
-                                                <td><?php echo $customer['id']; ?></td>
-                                                <td><?php echo $customer['name']; ?></td>
-                                                <td><?php echo $customer['cash']; ?></td>
-                                                <td><?php echo $customer['change']; ?></td>
-                                                <td><?php echo $customer['totalpay']; ?></td>
-
+                                                <td>
+                                                    <?= $stock['name'] ?>
+                                                </td>
+                                                <td>
+                                                ₱ <?= number_format($stock['retail'], 2) ?>
+                                                </td>
+                                                <td>
+                                                    <?= $stock['sold'] ?>
+                                                </td>
+                                                <td>
+                                                    <?= $stock['available'] ?>
+                                                </td>
                                             </tr>
-
-                                        <?php endforeach; ?>
-                                        <!-- <tr>
-                  <th scope="row">1</th>
-                  <td>Mark</td>
-                  <td>Otto</td>
-                  <td>@mdo</td>
-                </tr>
-                <tr>
-                  <th scope="row">2</th>
-                  <td>Jacob</td>
-                  <td>Thornton</td>
-                  <td>@fat</td>
-                </tr>
-                <tr>
-                  <th scope="row">3</th>
-                  <td colspan="2">Larry the Bird</td>
-                  <td>@twitter</td>
-                </tr> -->
+                                        <?php endforeach ?>
                                     </tbody>
                                 </table>
                             </div>
